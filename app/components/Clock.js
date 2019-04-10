45c1e4b1dcab9a41a9678c68d095eed22f23b37f
Xun Zhou <xun.zhou@lidl.com>
2019-04-10 19:40:49 +0200

    Add clock component

---
+++ app/components/Clock.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import styles from './Clock.css';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timeId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeId);
+  }
+
+  tick() {
+    this.setState({date: new Date()});
+  }
+
+  render() {
+    const {date} = this.state;
+    return (
+      <div className={styles.time}>
+        {`${date.getUTCHours()}:${date.getUTCMinutes()}`}
+      </div>
+    );
+  }
+}
+
+export default Clock;
